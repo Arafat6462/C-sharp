5eb1416aaf9c2c83c3d80a8aa4251f84d332ec3b
Arafat...w <arafat6462@gmail.com>
2021-03-06 16:02:39 +0600

    c# mid assignment

--- sample_test_framework/Program.cs
+++ sample_test_framework/Program.cs
@@ -3,21 +3,40 @@ namespace sample_test_framework
 {
     class Program
     {
-        private int a;
-      /*  Program()
+
+        static int[] jobNumber = new int[5];
+        static int number;
+        public static int TakeInputJobNumber(Object obj, int jobIndex)
         {
-            Console.WriteLine("i'm in default constructor");
-        }*/
-        Program( int a=0)
-        {
-            this.a = a;
-            Console.WriteLine("i'm in parameterized constructor");
+            Console.Write(obj);
+              number = Convert.ToInt32(Console.ReadLine());
+
+            // is number already exist in array
+            for (int i = 0; i < jobIndex; i++)
+            {
+                if (jobNumber[i] == number)
+                {
+                    TakeInputJobNumber("Please enter job Number again : ", jobIndex);
+                }
+            }
+
+            jobNumber[jobIndex] = number;
+            return number;
+ 
         }
         static void Main(string[] args)
         {
-            Program program = new Program();
-            Console.ReadKey();
+            for(int i=0; i<5; i++)
+            {
+                Console.WriteLine(TakeInputJobNumber("Input : ", i));    
+            }
 
+            Console.WriteLine("\n-----------");
+            for (int i=0; i<jobNumber.Length; i++)
+            {
+                Console.WriteLine(jobNumber[i]);
+            }
+            Console.ReadKey();
         }
     }
 }
